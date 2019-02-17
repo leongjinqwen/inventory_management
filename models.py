eb9907b196907bd51cb27a1628270a45aa5a7b83
LeongJinqWen <jwleong@LeongJinqWens-MacBook-Pro.local>
2019-02-17 23:08:04 +0800

    added models and database

---
+++ models.py
@@ -0,0 +1,31 @@
+import os
+import peewee as pw
+import datetime
+from playhouse.postgres_ext import PostgresqlExtDatabase
+
+db = PostgresqlExtDatabase(os.getenv('DATABASE'))
+
+class BaseModel(pw.Model):
+    created_at = pw.DateTimeField(default=datetime.datetime.now)
+    updated_at = pw.DateTimeField(default=datetime.datetime.now)
+    
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.datetime.now()
+        return super(BaseModel,self).save(*args,**kwargs)
+
+    class Meta:
+        database = db
+        legacy_table_names = False
+
+class Store(BaseModel):
+    name = pw.CharField(unique=True)
+
+class Warehouse(BaseModel):
+    store = pw.ForeignKeyField(Store, backref='warehouses', unique=True)
+    location = pw.TextField()
+
+class Product(BaseModel):
+    name = pw.CharField(index=True)
+    description = pw.TextField()
+    warehouse = pw.ForeignKeyField(Warehouse, backref='products')
+    color = pw.CharField(null=True)
--- server.py
+++ server.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, request
+from models import db
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    db.connect()
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
 
 @app.route("/")
 def index():
