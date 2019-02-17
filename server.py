032f9a1dfbfbf31c6b29f826d78ff6468f3a160d
LeongJinqWen <jwleong@LeongJinqWens-MacBook-Pro.local>
2019-02-17 23:13:41 +0800

    migrate to database

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+import peeweedbevolve
 from flask import Flask, render_template, request
 from models import db
+
 app = Flask(__name__)
 
 @app.before_request
@@ -10,6 +12,10 @@ def before_request():
 def after_request(response):
     db.close()
     return response
+
+@app.cli.command()
+def migrate():
+    db.evolve(ignore_tables={'base_model'})
 
 @app.route("/")
 def index():
